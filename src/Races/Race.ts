ccd952e9153be633a357d62da32714f4abd2f0cc
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-05-05 16:08:30 -0300

    req 1

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -2,22 +2,23 @@ abstract class Race {
   private _name: string;
   private _dexterity: number;
 
-  constructor (_name: string, _dexterity: number) {
-    Race.createdRacesInstaces();
+  constructor(_name: string, _dexterity: number) {
+    Race.createdRacesInstances();
     this._name = _name;
     this._dexterity = _dexterity;
   }
 
   get name(): string {
-    return this._name
+    return this._name;
   }
 
   get dexterity(): number {
-    return this._dexterity
+    return this._dexterity;
   }
 
-  static createdRacesInstaces(): number {
-     throw new Error('Not implemented')
+  static createdRacesInstances(): number {
+    console.log('hiiii');
+    throw new Error('Not implemented');
   }
 
   abstract get maxLifePoints(): number;
